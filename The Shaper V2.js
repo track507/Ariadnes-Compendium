3df02df5292e02c037a5bd3897062071f809a7de
track507 <136283573+track507@users.noreply.github.com>
2024-05-14 23:39:12 -0700

    Finished 7th level feature

--- The Shaper V2.js
+++ The Shaper V2.js
@@ -67,6 +67,7 @@ ClassList["shaper"] = {
 		class : ["druid"],
 		level : [1, 5],
 	},
+    attacks : [1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
     features : {
         "fighting style" : {
             name : "Fighting Style",
@@ -100,7 +101,11 @@ ClassList["shaper"] = {
                         }
                     }
                 ]
-            }
+            },
+            limfeaname : "Imbue Element",
+            usages : "Prociency bonus per ",
+            usagescal : "event.value = How('Proficiency Bonus');",
+            recovery : "long rest"
         },
         "spellcasting" : {
             name : "Spellcasting",
@@ -136,7 +141,181 @@ ClassList["shaper"] = {
                     return n < 3 ? 0 : n < 6 ? 1 : n < 10 ? 2 : n < 14 ? 3 : 4;
                 }),
                 selection : ["druidcraft"]
-            }]
+            }],
+            extraname : "Signs of Poiwer",
+			extrachoices : ["Disturbance", "Elemental Protection", "Force Shift", "Reinforce Body", "Arrest Momentum", "Zone of Power", "Purge Infusion", "Elemental Boon", "Preserve Infusion", "Quicken", "Reinforce Will", "Sharp Winds", "Force Step", "Interruption", "Fire Wake", "Extend Infusion"],	
+            extraTimes : leves.map(function(n) {
+                return n < 6 ? 0 : n < 10 ? 1 : n < 14 ? 2 : 3;
+            }),
+            "disturbance" : {
+                name : "Disturbance",
+                source : [["A:TSV2", 15]],
+                description :   "Casting Time: 1 bns\n"
+                                + "Range: Self (15-ft cone)\n"
+                                + "Components: S\n"
+                                + "Duration: Instantaneous\n"
+                                + "Medium or smaller crea(s) Strength save or pushed 5 ft away and take 1d4 force dmg"
+            },
+            "elemental protection" : {
+                name : "Elemental Protection",
+                source : [["A:TSV2", 15]],
+                description :   "Casting Time: 1 bns\n"
+                                + "Range: Self\n"
+                                + "Components: S\n"
+                                + "Duration: 1 rnd\n"
+                                + "Gain resistance to acid, cold, fire, lightning, or thunder"
+            },
+            "force shift" : {
+                name : "Force Shift",
+                source : [["A:TSV2", 15]],
+                description :   "Casting Time: 1 bns\n"
+                                + "Range: Touch\n"
+                                + "Components: S\n"
+                                + "Duration: Conc, 1 min\n"
+                                + "1 crea gains advantage on next Str, Dex, or Con save"
+            },
+            "arrest momentum" : {
+                name : "Arrest Momentum",
+                source : [["A:TSV2", 15]],
+                description :   "Casting Time: 1 rea when crea/obj in motion is about to crash\n"
+                                + "Range: 30 ft\n"
+                                + "Components: S\n"
+                                + "Duration: 1 rnd\n"
+                                + "Medium or smaller crea/obj momentum nullified (spd 0) and hanging in the air for the duration; target drops to ground when sign fades"
+            },
+            "zone of pwoer" : {
+                name : "Zone of Power",
+                source : [["A:TSV2", 15]],
+                description :   "Casting Time: 1 bns\n"
+                                + "Range: Self (10-ft rad)\n"
+                                + "Components: S\n"
+                                + "Duration: Conc, 1 min\n"
+                                + "I gain temp hp equal to my Wis mod at start of each of my turns"
+            },
+            "purge infusion" : {
+                name : "Purge Infusion",
+                source : [["A:TSV2", 15]],
+                description :   "Casting Time: 1 bns\n"
+                                + "Range: Self (30-ft rad)\n"
+                                + "Components: S\n"
+                                + "Duration: Instantaneous\n"
+                                + "All crea(s) Con save or blinded til end of their next turn"
+            },
+            "elemental boon" : {
+                name : "Elemental Boon",
+                source : [["A:TSV2", 15]],
+                description :   "Casting Time: 1 bns\n"
+                                + "Range: 30 ft\n"
+                                + "Components: S\n"
+                                + "Duration: Conc, 1 min\n"
+                                + "Harness fire, earth, water, air; see notes",
+                toNotesPage : [{
+                    name : "Harness Elemental Boon",
+                    note : desc([
+                        "Fire: I gain an additional 1d6 fire damage on my next weapon attack",
+                        "Earth: I gain resistance to bludgeon., pierce., and slash. damage dealt by the next attack that hit me",
+                        "Water: If I am below half my max hp, I heal 1d6 hp",
+                        "Air: My next ranged weapon attack doubles its range"
+                    ])
+                }]
+            },
+            "preserve infusion" : {
+                name : "Preserve Infusion",
+                source : [["A:TSV2", 16]],
+                submenu : "[shaper level 6+]",
+                description :   "Casting Time: 1 bns\n"
+                                + "Range: Self\n"
+                                + "Components: S\n"
+                                + "Duration: Conc, 1 hr\n"
+                                + "If infused weapon over half of its duration left, I interrupt it for the duration. When spell ends, or I end it, infusion resumes with 5 rounds left.",
+                prereqeval : function(v) { return classes.known.shaper.level >= 6 }
+            },
+            "quicken" : {
+                name : "Quicken",
+                source : [["A:TSV2", 16]],
+                submenu : "[shaper level 6+]",
+                description :   "Casting Time: 1 bns\n"
+                                + "Range: Self\n"
+                                + "Components: S\n"
+                                + "Duration: Instantaneous\n"
+                                + "I can take the dodge, help, dash, shove, grapple, or disengage as part of this spell's casting",
+                prereqeval : function(v) { return classes.known.shaper.level >= 6 }
+            },
+            "reinforce will" : {
+                name : "Reinforce Will",
+                source : [["A:TSV2", 16]],
+                submenu : "[shaper level 6+]",
+                description :   "Casting Time: 1 bns\n"
+                                + "Range: Touch\n"
+                                + "Components: S\n"
+                                + "Duration: Conc, 1 min\n"
+                                + "1 crea advantage on next Int, Wis, or Cha save",
+                prereqeval : function(v) { return classes.known.shaper.level >= 6 }
+            },
+            "sharp winds" : {
+                name : "Sharp Winds",
+                source : [["A:TSV2", 16]],
+                submenu : "[shaper level 6+]",
+                description :   "Casting Time: 1 rea when crea misses me with a melee attack\n"
+                                + "Range: Touch\n"
+                                + "Components: S\n"
+                                + "Duration: Instantaneous\n"
+                                + "Melee spell atk; 1d8 + Wis mod slashing dmg; +1d8 at CL 5, 11, and 17",
+                prereqeval : function(v) { return classes.known.shaper.level >= 6 && (/\bwindchaser\b/).test(classes.known.shaper.subclass); }
+            },
+            "force step" : {
+                name : "Force Step",
+                source : [["A:TSV2", 16]],
+                submenu : "[shaper level 10+]",
+                description :   "Casting Time: 1 bns\n"
+                                + "Range: Self\n"
+                                + "Components: S\n"
+                                + "Duration: rnd\n"
+                                + "Move across vertical/liquid surfaces on my turn without falling",
+                prereqeval : function(v) { return classes.known.shaper.level >= 10 }
+            },
+            "interruption" : {
+                name : "Interruption",
+                source : [["A:TSV2", 16]],
+                submenu : "[shaper level 10+]",
+                description :   "Casting Time: 1 rea when crea casts 5th level or lower spell\n"
+                                + "Range: Touch\n"
+                                + "Components: S\n"
+                                + "Duration: Instantaneous\n"
+                                + "Melee spell atk; on hit, Con save or fail to cast; no effect SL>5",
+                prereqeval : function(v) { return classes.known.shaper.level >= 10 }
+            },
+            "fire wake" : {
+                name : "Fire Wake",
+                source : [["A:TSV2", 16]],
+                submenu : "[shaper level 10+]",
+                description :   "Casting Time: 1 bns\n"
+                                + "Range: Self\n"
+                                + "Components: S\n"
+                                + "Duration: Conc, 1 min\n"
+                                + "Leave 5ft wide, 60ft long trail behind me; crea(s) within 5ft of trail, Dex save or 2d6 fire dmg; crea(s) crossing the trail take 2d6 fire dmg",
+                prereqeval : function(v) { return classes.known.shaper.level >= 10 && (/\bflamecaller\b/).test(classes.known.shaper.subclass)}
+            },
+            "extend infusion" : {
+                name : "Extend Infusion",
+                source : [["A:TSV2", 16]],
+                submenu : "[shaper level 14+]",
+                description :   "Casting Time: 1 bns\n"
+                                + "Range: Self\n"
+                                + "Components: S\n"
+                                + "Duration: Instantaneous\n"
+                                + "Infusion extended to 10 min; can only be used once per infusion",
+                prereqeval : function(v) { return classes.known.shaper.level >= 14 }
+            },
+        },
+        "unwavering resolve" : {
+            name : "Unwavering Resolve",
+            source : [["A:TSV2", 9]],
+            description : desc([
+                "My Wisdom increases by 1, and my proficiency bonus is double for my Wisdom saving throws. This increases by +2 again at 18th level, to a max of 22"
+            ]),
+            scorestxt : "+1 Wisdom (max 20), and +2 at 18th level (max 22)",
+            addMod : [{ type : "save", field : "Wis", mod : "max(Prof|0)", text : "I double my Proficiency bonus when calculating my Wisdom saving throws." }],
         }
     }
 }
