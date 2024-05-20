c172dfaada92e410d241e8c55fff00e4932f175c
track507 <136283573+track507@users.noreply.github.com>
2024-05-20 12:50:15 -0700

    Changed filename for formatting

--- The Better Spellbook V1.0.js
+++ The Better Spellbook V1.0.js
@@ -1,4 +1,4 @@
-var iFileName = "Ariadne's_Better_Spellbook";
+var iFileName = "Ariadne's:Better_Spellbook";
 RequiredSheetVersion("13.1.13");
 
 //The better spellbook v1.0 addition
@@ -12,7 +12,7 @@ SourceList["A:BS"] = {
 
 /*
 
-    ANY SPELLS THAT ARE ON OTHER PDPFS OR COMPENDIUMS WILL BE LISTED HERE AND COMMENTED OUT TO PREVENT OVERLAPPING SPELLS
+    ANY SPELLS THAT ARE ON OTHER PDFS OR COMPENDIUMS WILL BE LISTED HERE AND COMMENTED OUT TO PREVENT OVERLAPPING SPELLS
 
 */
 
@@ -71,7 +71,7 @@ SpellsList["visions of doom"] = {
     duration : "Conc, 1 hr",
     compMaterial : "a crystal sphere",
     ritual : true,
-    description : "After casting, choose Doomsight or Criticlal Failure",
+    description : "After casting, choose Doomsight or Critical Failure; see B.",
     descriptionFull : "Once you finish casting this spell, choose one of the followng effects. Doomsight: At the DM's discretion, you witness the worst possible outcomes you could encounter by taking a specific coure of action. You can use this foretelling power a number of times equal to your spellcasting ability modifier. Critical Failure: A terrible future is revealed to you. You may replace any attack roll, saving throw or ability check made by a creature you can see with a one. This effect expires if not used during the duration of the spell."
 };
 SpellsList["eldritch oblation"] = {
@@ -86,9 +86,25 @@ SpellsList["eldritch oblation"] = {
     duration : "Instantaneous",
     compMaterial : "one or more humanoid creatures, which the spell consumes",
     ritual : true,
-    description : "Gain a Gift; can't regain a gift unless it's servitude; DM's Discretion; see desc.",
+    description : "Gain a Gift; can't regain a gift unless it's servitude; DM's Discretion; see B.",
     descriptionFull : "You can sacrifice a number of creatures up to your spellcasting ability modifier. Under the discretion of the DM, you may ask for a great boon from the beyond in proportion to your sacrifice; not all souls are weighted equal. These eldritch blessings can take froms of the following. Gift of Form: You can use your action to shapechange into a creature you sacrified. You use the creature's statistics and abilities, but keep your Intelligence. Your HP is whichever is greater. Gift of Servitude: You gain a feat chosen by the DM, can cast this spell at 6th level, and this boon turns you into a Harbringer Shaman (or even Harbringer Queen depending on the value of the sacrifices). Gift of Eyes: You can now cast Foresight once per day without expending a spell slot. Gift of Blood: You gain half your current max HP as temp. HP after every long rest, one fourth after every short rest. These HP do not stack after a rest, the max you can have is half of your max HP. These boons are just examples. You cannot gain the benefits of a boon twice unless it's servitude. After casting this spell, you gain an unexplainable affliction; bad luck, forgetting loved ones, or always feeling euphoric. The DM chooses your affliction"
 };
+if(!SpellsList["thunderous crimson"]) {
+    SpellsList["thunderous crimson"] = {
+        name : "Thunderous Crimson",
+        source : [["A:BS", 6]],
+        level : 1,
+        school : "Evoc",
+        classes : ["bard", "cleric", "druid", "paladin", "ranger", "sorcerer", "warlock", "wizard"],
+        time : "1 a",
+        range : "S:30-ft cone",
+        components : "V,S",
+        duration : "Instantaneous",
+        save : "Dex",
+        description : "4d4+2d4/SL Lightning dmg, dazzled until end of their next turn; save no dmg, not dazzled",
+        descriptionFull : "You summon a wave of crackling red lightning that travels in a 30 foot cone in front of you. Each crea in that area must succeed a Dexterity save or suffer 4d4 lightning damage and be dazzled until the end of their next turn" + AtHigherLevels + "When you cast this spell using a spell slot of 2nd level or higher, the damage increases by 2d4 for each slot about 1st level."
+    };
+}
 SpellsList["ariadne's mimicry"] = {
     name : "Ariadne's Mimicry",
     source : [["A:BS", 6]],
@@ -104,6 +120,22 @@ SpellsList["ariadne's mimicry"] = {
     description : "Save or transform crea with at least 1 HP into object; Transforms back as bns or 10 damage dealt",
     descriptionFull : "A creature that you can see within range with at least 1HP makes a Wisdom saving throw or transforms into a common object, large or smaller. A shapechanger automatically succeeds on the save. This transformation lasts for the duration or until the creature takes 10 damage. The creatures statistsics are replaced with the chosen object, but it retains it alignment and personality. You can use your bonus action to transform the creature back to its original form and have it act immediately. This creature is also unable to speak, cast spells, or take any action that requires hands or speech. Their gear also melds into the chosen object, and can't activate, use, wield, or otherwise benefit from any of its equipment. While in object form, the creature can move up to half its speed by magical means."
 };
+if(!SpellsList["death and despair"]) {
+    SpellsList["death and despair"] = {
+        name : "Death and Despair",
+        source : [["A:BS", 6]],
+        level : 8,
+        school : "Necro",
+        classes : ["bard", "cleric", "druid", "paladin", "ranger", "sorcerer", "warlock", "wizard"],
+        time : "1 a",
+        range : "90 ft",
+        components : "V,S",
+        duration : "Conc, 1 min",
+        save : "Con",
+        description : "30ft wide, 10ft tall cylinder; all crea 12d6 Necrotic dmg and demoralized; save halves and not demoralized; see desc.",
+        descriptionFull : "Choose a 30 ft radius surface you can see within range. All creatures that are within 10 ft high of the surface must make a Constitution saving throw taking 12d6 Necrotic damage, or half as much on a save. A creature must make a save when it enters the spells area for the first time or ends its turn there. Nonmagical plants with and die, while magical plants receive double damage. All creatures damaged by this spell at least twice must make a Wisdom saving throw or become demoralized. A demoralized creature has its speed reduced to 0 and falls to its knees in absolute despair."
+    };
+}
 SpellsList["runo's coin throw"] = {
     name : "Runo's Coin Throw",
     source : [["A:BS", 7]],
@@ -115,6 +147,49 @@ SpellsList["runo's coin throw"] = {
 	components : "S,M",
     compMaterial : "a coin which the spell consumes",
     duration : "Instantaneous",
-    description : "Make ranged spell atk; 3d8 Force dmg; Choose silver, gold, or platinum coin to consume (see desc)",
+    description : "Make ranged spell atk; 3d8 Force dmg; Choose silver, gold, or platinum coin to consume; see B.",
     descriptionFull : "When you cast this spell, you can select the type of coin coin you want to consume; silver, gold, or platinum. All coins deal 3d8 force damage on a successful hit. A silver coin only deals damage. A gold coin deals the damage and the target must succeed on a Strength saving throw or be restrained until the end of their next turn. A platinum coin deals the damage and the target must make a Charisma saving throw or have a single nonmagical possession of your choice transported to an unreachable extraplanar space, reappearing after one minute has passed. This can be a suit of armor, weapons, a purse, etc."
 };
+SpellsList["crystal shards"] = {
+    name : "Crystal Shards",
+    source : [["A:BS", 7]],
+	level : 1,
+    school : "Evoc",
+    classes : ["bard", "cleric", "druid", "paladin", "ranger", "sorcerer", "warlock", "wizard"],
+	time : "1 a",
+    range : "90 ft",
+	components : "V,S,M",
+    compMaterial : "a starshard",
+    duration : "Instantaneous",
+    save : "Dex",
+    description : "20ft rad; Each crea save or 1d6 pierce dmg and irradiated for 1d4+1 turns; see B.",
+    descriptionFull : "Each creature in a 20 ft radius sphere centered on a point within range must make a Dexterity saving throw or take 1d6 piercing damage and be irradiated for 1d4+1 turns. Irradiated creatures take an additional 1d6 force damage whenever they are affected by a spell or magical effect."
+};
+SpellsList["fallow's theorem"] = {
+    name : "Fallow's Theorem",
+    source : [["A:BS", 7]],
+	level : 1,
+    school : "Div",
+    classes : ["bard", "cleric", "druid", "paladin", "ranger", "sorcerer", "warlock", "wizard"],
+	time : "1 bns",
+    range : "Self",
+	components : "V,S",
+    duration : "Conc, 5 min",
+    description : "See up to 5 min of consequences for a 1 action I take in next hr; Adv on seen saves; cannot be suprised",
+    descriptionFull : "I summon a crystal shard of arcane divination, which I can utilize to peer into possible futures. I may envision the consequecnes of a single action I could take in the next hour, witnessing up to 5 minute's worth of likely consequences before the shard crumbles. If I witnessed any effects that would cause me to make a saving throw in my vision and decide to go through with the action, I gain advantage on the roll. Additionally, I cannot be surprised by creatures whose actions I witnessed inside the shard."
+}
+SpellsList["energy feedback"] = {
+    name : "Energy Feedback",
+    source : [["A:BS", 7]],
+	level : 4,
+    school : "Necro",
+    classes : ["bard", "cleric", "druid", "paladin", "ranger", "sorcerer", "warlock", "wizard"],
+	time : "1 a",
+    range : "Touch",
+	components : "V,S,M",
+    compMaterial : "a charged starshard",
+    duration : "Instantaneous",
+    save : "Cha",
+    description : "Melee spell atk, takes 4d6+1d6/SL force and save or +4d6 force dmg; see B.",
+    descriptionFull : "I make a melee spell attack against a creature within my reach. On a hit, the target takes 4d6 force dmg and must make a Charisma saving or take an additional 4d6 force damage. A spellcaster that fails this saving throw loses a 3rd level spell slot, and is unable to cast spells from a school of my choice until the end of their next turn." + AtHigherLevels + "When I cast this spell using a spell slot of 5th level or higher, my attack increased by 1d6 for each slot level above 4th. In addition, spellcasters who fail the saving throw are drained of a spell slot one level below the spell slot that was used to cast this spell."
+}
